0983b52d781c7d8018ec2e069917970a5ba81a57
rgoliveira <rgoliveira1441973@gmail.com>
2023-07-01 16:12:27 -0300

    feat: create home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FontAwesome5, Feather } from '@expo/vector-icons';
+
+import { BackButton, Container, MarginContainer, Title, TopSection, SearchBar, SearchInput } from "./styles"
+import { Coin } from '../../components/Coin';
+import { useCoin } from '../../hooks/home/useCoin';
+
+export const Home: React.FC = () => {
+  const { searchText, handleSearch, handleRefresh, refreshing, filteredCoins, handleBackButton } = useCoin()
+
+  return (
+    <Container>
+      <MarginContainer>
+        <TopSection>
+          <BackButton onPress={handleBackButton}>
+            <Feather name="arrow-left" size={28} color="black" />
+          </BackButton>
+          <Title>Crypto</Title>
+        </TopSection>
+        <SearchBar>
+          <FontAwesome5 name="search" size={18} color="#222" />
+          <SearchInput
+            placeholder="Search crypto"
+            value={searchText}
+            onChangeText={handleSearch}
+          />
+        </SearchBar>
+      </MarginContainer>
+
+      <Coin
+        refreshing={refreshing}
+        handleRefresh={handleRefresh} 
+        filteredCoins={filteredCoins}
+      />
+    </Container>
+  );
+};
